3587c18c090b8a9993d29a5c9e06a63714ee0452
Tsukasa Nishiyama <tuka@trkg.jp>
2018-07-16 14:39:52 +0900

    :art: auto scaling

---
+++ terraform/autoscaling.tf
@@ -0,0 +1,44 @@
+module "front-asg" {
+  source = "terraform-aws-modules/autoscaling/aws"
+
+  name = "front-web"
+
+  lc_name = "handson-front-lc"
+
+  image_id        = "ami-e99f4896"
+  instance_type   = "t2.micro"
+  key_name        = "${aws_key_pair.bastion.key_name}"
+  security_groups = ["${module.allow-http-sg.this_security_group_id}"]
+
+  ebs_block_device = [
+    {
+      device_name           = "/dev/xvdz"
+      volume_type           = "gp2"
+      volume_size           = "16"
+      delete_on_termination = true
+    },
+  ]
+
+  root_block_device = [
+    {
+      volume_size = "16"
+      volume_type = "gp2"
+    },
+  ]
+
+  asg_name                  = "handson-front-asg"
+  vpc_zone_identifier       = ["${module.vpc.private_subnets}"]
+  health_check_type         = "EC2"
+  min_size                  = 1
+  max_size                  = 6
+  desired_capacity          = 3
+  wait_for_capacity_timeout = 0
+
+  tags = [
+    {
+      key                 = "Environment"
+      value               = "handson"
+      propagate_at_launch = true
+    },
+  ]
+}
--- terraform/security-group.tf
+++ terraform/security-group.tf
@@ -7,3 +7,13 @@ module "allow-ssh-sg" {
 
   ingress_cidr_blocks = ["${var.allow_ssh_ip}"]
 }
+
+module "allow-http-sg" {
+  source = "terraform-aws-modules/security-group/aws//modules/http-80"
+
+  name        = "allow-http-sg"
+  description = "allow http access"
+  vpc_id      = "${module.vpc.vpc_id}"
+
+  ingress_cidr_blocks = ["${var.vpc_cidr}"]
+}
